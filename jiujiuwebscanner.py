9e017bbb755898569bb02089f69aa459129367fc
jiujiuyao <jiujiuwhoami@outlook.com>
2023-06-29 19:51:39 +0800

    update 1.0.0

--- jiujiuwebscanner.py
+++ jiujiuwebscanner.py
@@ -1,33 +1,34 @@
 
+import wx
 import argparse
 import concurrent.futures
 import os
 import importlib
 from modules import VulnerabilityScanner as vulscanner
+from modules import VulnerabilityScannerGUI as vulscannergui
 def parse_arguments():
     parser = argparse.ArgumentParser(description='漏洞扫描器')
-    parser.add_argument('-t', '--targets', nargs='+', help='目标urls/ips ，Example : http://baidu.com?id=1', required=True)
+    parser.add_argument('-t', '--targets', nargs='+', help='目标urls/ips ，Example : http://baidu.com?id=1', required=False)
     parser.add_argument('-f', '--files', nargs='+', help='POCs文件', required=False)
     parser.add_argument('-n', '--threads', type=int, help='最大线程', default=1)
-    parser.add_argument('-T', '--target-file', help='多目标urls/ips文件')
-    parser.add_argument('-F', '--folder', help='POCs文件夹')
+    parser.add_argument('-T', '--target_file', help='多目标urls/ips文件')
+    parser.add_argument('-F', '--poc_folder', help='POCs文件夹')
+    parser.add_argument('--gui', action='store_true', help='启动GUI界面')
+    #parser.set_defaults(gui=True)  
     return parser.parse_args()
 def main():
     args = parse_arguments()
-    scanner = vulscanner.VulnerabilityScanner([], args.files, args.threads)
-    # 如果有ips文件，则重新赋值
-    if args.target_file:
-        with open(args.target_file, 'r') as file:
-            targets = file.read().splitlines()
-        scanner.targets = targets
-    elif args.targets:
-        scanner.targets = args.targets
-
-    if args.folder:
-        scanner.load_vulnerabilities_from_folder(args.folder)
-    
-    scanner.run_detection()
-    print("【+】扫描完成")
+    scanner = vulscanner.VulnerabilityScanner(targets=args.targets, target_files=args.target_file,files=args.files, files_pocs=args.poc_folder,threads=args.threads)
+    if args.gui:
+        app = wx.App()
+        
+        frame = vulscannergui.VulnerabilityScannerGUI(None, title="jiujiuwebscanner")
+        frame.Show()
+        app.MainLoop()
+    else:
+        scanner.run_detection()
+        
 
 if __name__ == '__main__':
     main()
+    print("【+】扫描完成")
--- pocs/sql/SqlTimeInject.py
+++ pocs/sql/SqlTimeInject.py
@@ -2,6 +2,7 @@ import requests
 
 def detect_vulnerability(target):
     url = target
+    result  = False
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
     payload = "' AND (SELECT sleep(5))--"
     try:
